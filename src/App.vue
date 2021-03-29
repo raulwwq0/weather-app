1779e2e290bc8f0db7f423cd71ed81c65b8f0c8b
Raul <ag21raul@gmail.com>
2021-03-29 13:50:10 +0200

    Starting sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Sidebar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from "./components/Sidebar";
+import "./assets/styles/css/styles.css";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="sidebar">
+        <div id="sidebar-buttons">
+            <button id="search-button">Search for places</button>
+            <button id="location-button"></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+}
+</script>
